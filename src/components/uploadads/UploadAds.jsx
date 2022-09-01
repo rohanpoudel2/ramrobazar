cc825c6386be91f94cf316e7d288b2a345d3591c
Rohan Poudel <nsrapoudel@gmail.com>
2022-09-01 14:28:39 +0545

    done creating add ads modal

--- src/components/uploadads/UploadAds.jsx
+++ src/components/uploadads/UploadAds.jsx
@@ -6,10 +6,14 @@ import Modal from '@mui/material/Modal';
 import CloseIcon from '@mui/icons-material/Close';
 import CircularProgress from '../circularprogress/CircularProgress';
 import AddIcon from '@mui/icons-material/Add';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 import { useDispatch } from 'react-redux'
 import { open, close } from '../../redux/AddPostSlice'
+import { Divider, Switch } from '@mui/material';
 
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import GenericProductCard from '../genericproductcard/GenericProductCard'
 
 const style = {
   position: 'absolute',
@@ -21,17 +25,26 @@ const style = {
   width: '50vw',
   height: '75vh',
   borderRadius: '10px',
-  outline: 'none'
+  outline: 'none',
+  overflow: 'scroll'
 };
 
 
 const UploadAds = ({ toggle }) => {
 
+  const [stepCounter, setStepCounter] = React.useState(1);
+  console.log(stepCounter)
   const openn = toggle;
 
   const dispatch = useDispatch()
   const handleOpen = () => dispatch(open());
   const handleClose = () => dispatch(close());
+
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
   React.useEffect(() => {
     if (toggle) {
@@ -39,7 +52,7 @@ const UploadAds = ({ toggle }) => {
     } else {
       handleClose()
     }
-  }, [toggle])
+  }, [toggle, stepCounter])
   console.log(toggle)
   return (
     <div className="uploadads">
@@ -48,13 +61,14 @@ const UploadAds = ({ toggle }) => {
         onClose={handleClose}
       >
         <Box sx={style} className='box'>
+
           <div className="top">
             <div className="top-bar">
               <span>Post Ads</span>
               <CloseIcon className='icon' onClick={() => handleClose()} />
             </div>
             <div className="bottom-bar">
-              <CircularProgress step='1' />
+              <CircularProgress step={stepCounter} />
               <div className="text">
                 <span>Upload Photo</span>
                 <span>Next Step: Description</span>
@@ -62,22 +76,112 @@ const UploadAds = ({ toggle }) => {
             </div>
           </div>
           <div className="bottom">
-            <div className="b-top">
-              <input type="text" placeholder='Ad Title *' />
-              <div className="rnd">
-                <span>For example: Brand, Model, Color, and size</span>
-                <span className="counter">0/50</span>
+            {stepCounter === 1 &&
+              <div className="b-top">
+
+                <input type="text" placeholder='Ad Title *' />
+                <div className="rnd">
+                  <span>For example: Brand, Model, Color, and size</span>
+                  <span className="counter">0/50</span>
+                </div>
+                <div className="upload">
+                  <input type="file" id='file' name='file' hidden />
+                  <label htmlFor="file">
+                    <AddIcon className='icon' />
+                  </label>
+                  <span>Choose a photo *</span>
+                </div>
               </div>
-              <div className="upload">
-                <input type="file" id='file' name='file' hidden />
-                <label htmlFor="file">
-                  <AddIcon className='icon' />
-                </label>
-                <span>Choose a photo *</span>
+            }
+            {
+              stepCounter === 2 &&
+              <div className="second-step">
+                <select name="category" id="category">
+                  <option selected disabled>Category *</option>
+                  <option value="volvo">Volvo</option>
+                  <option value="saab">Saab</option>
+                  <option value="opel">Opel</option>
+                  <option value="audi">Audi</option>
+                </select>
+                <select name="condition" id="condition">
+                  <option selected disabled>Condition *</option>
+                  <option value="volvo">Volvo</option>
+                  <option value="saab">Saab</option>
+                  <option value="opel">Opel</option>
+                  <option value="audi">Audi</option>
+                </select>
+                <Divider />
+                <div className="location">
+                  <input type="text" placeholder='Entire Nepal' />
+                  <LocationOnIcon className='icon' />
+                </div>
+                <div className="hide">
+                  <input type="checkbox" />
+                  <label>Hide my precise location.</label>
+                </div>
+                <div className="desc">
+                  <input type="text" placeholder='Description *' />
+                </div>
+                <div className="type">
+                  <div className="type-item">
+                    <span>Delivery</span>
+                    <Switch
+                      checked={checked}
+                      onChange={handleChange}
+                      inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                  </div>
+                  <div className="type-item">
+                    <span>Is Adult Content?</span>
+                    <Switch
+                      checked={checked}
+                      onChange={handleChange}
+                      inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div>
-              <button>Next</button>
+            }
+            {
+              stepCounter === 3 &&
+              <div className="third-step">
+                <input type="text" placeholder='रु Price *' />
+                <span>Double check the price value you keep.</span>
+                <div className="subhead">
+                  Expiry Date
+                </div>
+                <select name="expiry" id="expiry">
+                  <option value="1 Month">1 Month</option>
+                  <option value="10 Days">10 Days</option>
+                </select>
+                <h3>Negotiable</h3>
+                <div className="type">
+                  <div className="type-item">
+                    <label>Can negotiate for price</label>
+                    <Switch
+                      checked={checked}
+                      onChange={handleChange}
+                      inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                  </div>
+                </div>
+              </div>
+            }
+            {
+              stepCounter === 4 &&
+              <div className="fourth-step">
+                <span>Your Ad will look like this</span>
+                <div className="ad-preview">
+
+                  <GenericProductCard />
+                </div>
+              </div>
+            }
+            <div className='buttons'>
+              {stepCounter !== 1 &&
+                <button className="backbtn" onClick={() => { setStepCounter(stepCounter - 1) }}><ArrowBackIcon className='icon' /> Back</button>
+              }
+              <button onClick={() => { setStepCounter(stepCounter + 1) }}>Next</button>
             </div>
           </div>
         </Box>
